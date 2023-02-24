f2ce4c338d4b02a73b1dd56ea2c61d447ca724d9
thecrazycoder1239 <tombarrett28@icloud.com>
2023-02-24 15:31:05 +0000

    adding hosting

--- listen.js
+++ listen.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-app.listen(9090, () => {
-    console.log(`listening on 9090...`);
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}...`);
   });
